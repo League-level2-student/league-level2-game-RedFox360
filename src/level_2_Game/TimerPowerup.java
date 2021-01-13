03ccd5c4f2c9238f0f77dee60664f613cfbd4bbc
Sameer Prakash <sameerprakash100@icloud.com>
2021-01-12 16:53:26 -0800

    01/12/2021

---
+++ src/level_2_Game/TimerPowerup.java
@@ -0,0 +1,43 @@
+package level_2_Game;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
+
+public class TimerPowerup extends GameObject {
+
+	public static BufferedImage image;
+	public static boolean needImage = true;
+	public static boolean gotImage = false;
+
+	public TimerPowerup(int x, int y, int width, int height) {
+		super(x, y, width, height);
+		if (needImage) {
+			loadImage("timer_powerup_image.png");
+		}
+	}
+
+	public void draw(Graphics g) {
+		if (gotImage) {
+			g.drawImage(image, x, y, width, height, null);
+		} else {
+			g.setColor(Color.WHITE);
+			g.fillRect(x, y, width, height);
+		}
+	}
+
+	void loadImage(String imageFile) {
+		if (needImage) {
+			try {
+				image = ImageIO.read(this.getClass().getResourceAsStream(imageFile));
+				gotImage = true;
+			} catch (Exception e) {
+
+			}
+			needImage = false;
+		}
+	}
+
+}
